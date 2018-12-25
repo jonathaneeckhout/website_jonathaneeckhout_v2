54cb6e25809d3505bb15b8e0ca2b0f875b356ed3
Jonathan Eeckhout <eeckhout.jonathan@gmail.com>
2018-12-25 22:36:49 +0100

    add get request handler

--- www/js/get_projects.js
+++ www/js/get_projects.js
@@ -1,5 +1,8 @@
+var projects;
+var filterFields;
+var tags;
+
 function showAllProjects() {
-    var projects = document.getElementsByClassName('project');
     var i;
     for (i = 0; i < projects.length; i++) {
         projects[i].style.display = "inline";
@@ -7,12 +10,8 @@ function showAllProjects() {
 }
 
 function filter() {
-    var filterFields = document.getElementsByClassName('filterField');
-    var projects = document.getElementsByClassName('project');
-    var tags  =  document.getElementsByClassName('tags');
     var i;
     var j;
-
     for (i = 0; i < projects.length; i++) {
         for (j = 0; j < filterFields.length; j++) {
             if(filterFields[j].checked) {
@@ -24,10 +23,43 @@ function filter() {
     }
 }
 
+function findGetParameter(parameterName) {
+    var result = null,
+        tmp = [];
+    var items = location.search.substr(1).split("&");
+    for (var index = 0; index < items.length; index++) {
+        tmp = items[index].split("=");
+        if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+    }
+    return result;
+}
+
+function setFilterField(filterField) {
+    var i;
+    for (i = 0; i < filterFields.length; i++) {
+        if(filterFields[i].value == filterField) {
+            filterFields[i].checked = true;
+        }
+    }
+}
+
+function handleGetRequest() {
+    var parameter = findGetParameter("filter");
+    if (filter) {
+        setFilterField(parameter);
+        filter();
+    }
+}
+
 document.addEventListener("DOMContentLoaded", function (event) {
+    projects = document.getElementsByClassName('project');
+    filterFields = document.getElementsByClassName('filterField');
+    tags  =  document.getElementsByClassName('tags');
+
     var _selector = document.querySelector('ul');
     _selector.addEventListener('change', function (event) {
         showAllProjects();
         filter();
     });
+    handleGetRequest();
 });
